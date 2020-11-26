f0097c059b97e4c669692f67c2d52b2f9b720abf
Andrew <gskll@protonmail.com>
2020-11-26 14:06:59 +0100

    feat: add accordion widget

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Accordion from "./Accordion";
 
 const App = (props) => {
-  return <div>App</div>;
+  const items = [
+    {
+      title: "What is React?",
+      content: "A frontend javascript framework",
+    },
+    {
+      title: "Why React?",
+      content: "React is a favorite JS library among engineers",
+    },
+    {
+      title: "How do you use React?",
+      content: "By creating components",
+    },
+  ];
+
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
 
 export default App;
