194e80977560021ba583b1ca380f04c514e67637
Andrew <gskll@protonmail.com>
2020-11-27 17:58:46 +0100

    feat: Add nav links and header
    - `Link` component overrides anchor tag page reload
    - onClick:
      - prevent default page reload
      - change url path to relevant href
      - create and dispatch navigation event
    
    - Route component
      - on initial render add event listener for navigation event
      - on navigation event: set currentPath state variable
        - to force rerender the component and update as necessary

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Search from "./Search";
 import Dropdown from "./Dropdown";
 import Translate from "./Translate";
 import Route from "./Route";
+import Header from "./Header";
 
 const items = [
   {
@@ -40,6 +41,7 @@ const App = () => {
 
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "./Link";
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link className="item" href="/">
+        Accordion
+      </Link>
+      <Link className="item" href="/list">
+        Search
+      </Link>
+      <Link className="item" href="/dropdown">
+        Dropdown
+      </Link>
+      <Link className="item" href="/translate">
+        Translate
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
